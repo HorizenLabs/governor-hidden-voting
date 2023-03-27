c5ed38c7bb339512275c73c7caae9d03810be3a4
Luca Giussani <lucagiussani@horizenlabs.io>
2023-03-27 11:31:24 +0200

    Initial smart contract implementation

---
+++ contracts/Crypto.sol
@@ -0,0 +1,254 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9;
+
+struct CurvePoint {
+    uint256 x;
+    uint256 y;
+}
+
+type Scalar is uint256;
+
+type Challenge is uint128;
+
+uint256 constant ORDER = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
+uint256 constant P = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
+
+uint256 constant EC_ADD_ADDRESS = 0x06;
+uint256 constant EC_ADD_INPUT_LENGTH = 0x80;
+uint256 constant EC_ADD_OUTPUT_LENGTH = 0x40;
+
+uint256 constant EC_MUL_ADDRESS = 0x07;
+uint256 constant EC_MUL_INPUT_LENGTH = 0x60;
+uint256 constant EC_MUL_OUTPUT_LENGTH = 0x40;
+
+struct EncryptedVote {
+    CurvePoint a;
+    CurvePoint b;
+}
+
+struct EncryptedTally {
+    EncryptedVote vote;
+    uint numVoters;
+}
+
+struct ProofSkKnowledge {
+    Scalar s;
+    Challenge c;
+}
+
+struct ProofCorrectDecryption {
+    Scalar s;
+    Challenge c;
+}
+
+struct ProofVoteWellFormedness {
+    Scalar r0;
+    Scalar r1;
+    Challenge c0;
+    Challenge c1;
+}
+
+error ProofVerificationFailure();
+
+function g() pure returns (CurvePoint memory) {
+    return CurvePoint({x: 1, y: 2});
+}
+
+function gNeg() pure returns (CurvePoint memory) {
+    return CurvePoint({x: 1, y: P - 2});
+}
+
+function ecAdd(
+    CurvePoint memory a,
+    CurvePoint memory b
+) view returns (CurvePoint memory result) {
+    bytes32[4] memory input;
+    input[0] = bytes32(a.x);
+    input[1] = bytes32(a.y);
+    input[2] = bytes32(b.x);
+    input[3] = bytes32(b.y);
+    assembly {
+        let success := staticcall(
+            gas(),
+            EC_ADD_ADDRESS,
+            input,
+            EC_ADD_INPUT_LENGTH,
+            result,
+            EC_ADD_OUTPUT_LENGTH
+        )
+        switch success
+        case 0 {
+            revert(0, 0)
+        }
+    }
+}
+
+function ecMul(
+    CurvePoint memory a,
+    Scalar s
+) view returns (CurvePoint memory result) {
+    bytes32[3] memory input;
+    input[0] = bytes32(a.x);
+    input[1] = bytes32(a.y);
+    input[2] = bytes32(Scalar.unwrap(s));
+    assembly {
+        let success := staticcall(
+            gas(),
+            EC_MUL_ADDRESS,
+            input,
+            EC_MUL_INPUT_LENGTH,
+            result,
+            EC_MUL_OUTPUT_LENGTH
+        )
+        switch success
+        case 0 {
+            revert(0, 0)
+        }
+    }
+}
+
+function fiatShamirChallenge(bytes memory data) pure returns (Challenge) {
+    return Challenge.wrap(uint128(uint256(keccak256(data))));
+}
+
+function scalar(Challenge challenge) pure returns (Scalar) {
+    return Scalar.wrap(uint256(Challenge.unwrap(challenge)));
+}
+
+function neg(Scalar s) pure returns (Scalar) {
+    return Scalar.wrap(ORDER - Scalar.unwrap(s));
+}
+
+function verifyVoteWellFormedness(
+    ProofVoteWellFormedness calldata proof,
+    EncryptedVote calldata vote,
+    CurvePoint storage pk
+) view {
+    CurvePoint memory a0 = ecAdd(
+        ecMul(g(), proof.r0),
+        ecMul(vote.a, neg(scalar(proof.c0)))
+    );
+    CurvePoint memory a1 = ecAdd(
+        ecMul(g(), proof.r1),
+        ecMul(vote.a, neg(scalar(proof.c1)))
+    );
+    CurvePoint memory b0 = ecAdd(
+        ecMul(pk, proof.r0),
+        ecMul(vote.b, neg(scalar(proof.c0)))
+    );
+    CurvePoint memory b1 = ecAdd(
+        ecMul(pk, proof.r1),
+        ecMul(ecAdd(vote.b, gNeg()), neg(scalar(proof.c1)))
+    );
+    Challenge c = fiatShamirChallenge(
+        bytes.concat(
+            bytes32(pk.x),
+            bytes32(pk.y),
+            bytes32(vote.a.x),
+            bytes32(vote.a.y),
+            bytes32(vote.b.x),
+            bytes32(vote.b.y),
+            bytes32(a0.x),
+            bytes32(a0.y),
+            bytes32(b0.x),
+            bytes32(b0.y),
+            bytes32(a1.x),
+            bytes32(a1.y),
+            bytes32(b1.x),
+            bytes32(b1.y)
+        )
+    );
+    // wrapping this into an unchecked block, since challenges use mod 2^128 arithmetics
+    unchecked {
+        if (
+            Challenge.unwrap(c) !=
+            Challenge.unwrap(proof.c0) + Challenge.unwrap(proof.c1)
+        ) {
+            revert ProofVerificationFailure();
+        }
+    }
+}
+
+function verifySkKnowledge(
+    ProofSkKnowledge calldata proof,
+    CurvePoint calldata pk
+) view {
+    CurvePoint memory v = ecAdd(
+        ecMul(g(), proof.s),
+        ecMul(pk, neg(scalar(proof.c)))
+    );
+    Challenge c = fiatShamirChallenge(
+        bytes.concat(bytes32(pk.x), bytes32(pk.y), bytes32(v.x), bytes32(v.y))
+    );
+    if (Challenge.unwrap(c) != Challenge.unwrap(proof.c)) {
+        revert ProofVerificationFailure();
+    }
+}
+
+function verifyCorrectDecryption(
+    ProofCorrectDecryption calldata proof,
+    EncryptedVote storage encryptedVote,
+    uint decryptedVote,
+    CurvePoint storage pk
+) view {
+    CurvePoint memory d = ecAdd(
+        encryptedVote.b,
+        ecMul(g(), neg(Scalar.wrap(decryptedVote)))
+    );
+    verifyCorrectDecryptionInternal(proof, encryptedVote, d, pk);
+}
+
+function verifyCorrectDecryptionInternal(
+    ProofCorrectDecryption calldata proof,
+    EncryptedVote storage encryptedVote,
+    CurvePoint memory d,
+    CurvePoint storage pk
+) view {
+    CurvePoint memory u = ecAdd(
+        ecMul(encryptedVote.a, proof.s),
+        ecMul(d, neg(scalar(proof.c)))
+    );
+    CurvePoint memory v = ecAdd(
+        ecMul(g(), proof.s),
+        ecMul(pk, neg(scalar(proof.c)))
+    );
+    Challenge c = fiatShamirChallenge(
+        bytes.concat(
+            bytes32(pk.x),
+            bytes32(pk.y),
+            bytes32(encryptedVote.a.x),
+            bytes32(encryptedVote.a.y),
+            bytes32(encryptedVote.b.x),
+            bytes32(encryptedVote.b.y),
+            bytes32(u.x),
+            bytes32(u.y),
+            bytes32(v.x),
+            bytes32(v.y)
+        )
+    );
+    if (Challenge.unwrap(c) != Challenge.unwrap(proof.c)) {
+        revert ProofVerificationFailure();
+    }
+}
+
+function initTally(EncryptedTally storage tally) {
+    tally.numVoters = 1;
+    tally.vote.a = g();
+    tally.vote.b = g();
+}
+
+function finalizeTally(EncryptedTally storage tally) {
+    tally.numVoters--;
+    tally.vote.a = ecAdd(tally.vote.a, gNeg());
+    tally.vote.b = ecAdd(tally.vote.b, gNeg());
+}
+
+function updateTally(
+    EncryptedTally storage tally,
+    EncryptedVote calldata vote
+) {
+    tally.numVoters++;
+    tally.vote.a = ecAdd(tally.vote.a, vote.a);
+    tally.vote.b = ecAdd(tally.vote.b, vote.b);
+}
---
+++ contracts/Voting.sol
@@ -0,0 +1,184 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9;
+
+import "./Crypto.sol";
+
+/// @title A basic contract for private voting.
+/// @notice This contract implements a very basic logic:
+/// - The contract must be re-deployed for each separate voting event.
+/// - The starting/stopping of the voting phase is not authomatized, but left to the authority.
+/// - There is no voter authentication. Every address can cast a vote (still double voting from
+///   the same address is prevented).
+/// - There is a single tallying entity in control of the secret key behind the voting event
+///   public key. This entity can easily deanonymize individual votes.
+/// - Only yes/no votes are possible.
+contract Voting {
+    enum Status {
+        INIT,
+        DECLARED,
+        VOTING,
+        TALLYING,
+        FINI
+    }
+
+    /// @notice The address authorized to declare the election public key, and start/stop the
+    /// voting period. It is set during deployment to the deployer address.
+    address authority;
+    /// @notice The current status of the voting event.
+    Status public status;
+    /// @notice The EC-ElGamal public key of the voting event, used for encrypting votes.
+    /// The corresponding secret key is known by the authority responsible for tallying.
+    CurvePoint pk;
+    /// @notice A running encrypted tally of the votes cast so far.
+    EncryptedTally encryptedTally;
+    /// @notice The result (i.e. number of yes) of the voting event. Becomes available after
+    /// tallying.
+    uint result;
+    /// @notice A map to keep track of the address which have already voted to prevent
+    /// double voting.
+    mapping(address => bool) addressAlreadyVoted;
+    /// @notice A map to keep track of the proofs of vote well-formedness already sent, to
+    /// prevent possible attacks on ballot privacy.
+    mapping(bytes32 => bool) proofHashAlreadySeen;
+
+    event VotingStarted(CurvePoint pk);
+    event EncryptedVoteCast(address voter);
+    event VotingStopped(EncryptedTally tally);
+    event Result(uint result);
+
+    error Unauthorized(address caller);
+    error WrongStatus(Status status);
+    error DoubleVoting(address caller);
+    error DoubleProof();
+
+    constructor() {
+        authority = msg.sender;
+        status = Status.INIT;
+    }
+
+    /// @notice Declare the EC-ElGamal public key of the voting event. Can only be called by
+    /// authority.
+    /// @param pk_ The public key
+    /// @param proof The proof of knowledge of the secret key behind pk_. This is generated
+    /// off-chain by the cryptographic backend.
+    function declarePk(
+        CurvePoint calldata pk_,
+        ProofSkKnowledge calldata proof
+    ) external {
+        if (status != Status.INIT) {
+            revert WrongStatus(status);
+        }
+        if (msg.sender != authority) {
+            revert Unauthorized(msg.sender);
+        }
+        verifySkKnowledge(proof, pk_);
+
+        pk = pk_;
+        status = Status.DECLARED;
+    }
+
+    /// @notice Start the voting phase. Can only be called by authority.
+    function startVotingPhase() external {
+        if (status != Status.DECLARED) {
+            revert WrongStatus(status);
+        }
+        if (msg.sender != authority) {
+            revert Unauthorized(msg.sender);
+        }
+
+        initTally(encryptedTally);
+        status = Status.VOTING;
+
+        emit VotingStarted(pk);
+    }
+
+    /// @notice Cast an encrypted yes/no vote. Can be called by anyone.
+    /// @param vote The vote, encrypted via EC-Elgamal using public key pk
+    /// @param proof The proof of well-formedness of vote. This is generated
+    /// off-chain by the cryptographic backend.
+    function castVote(
+        ProofVoteWellFormedness calldata proof,
+        EncryptedVote calldata vote
+    ) external {
+        if (status != Status.VOTING) {
+            revert WrongStatus(status);
+        }
+        if (addressAlreadyVoted[msg.sender]) {
+            revert DoubleVoting(msg.sender);
+        }
+        bytes32 proofHash = keccak256(
+            bytes.concat(
+                bytes32(Scalar.unwrap(proof.r0)),
+                bytes32(Scalar.unwrap(proof.r1)),
+                bytes16(Challenge.unwrap(proof.c0)),
+                bytes16(Challenge.unwrap(proof.c1))
+            )
+        );
+        if (proofHashAlreadySeen[proofHash]) {
+            revert DoubleProof();
+        }
+        verifyVoteWellFormedness(proof, vote, pk);
+
+        updateTally(encryptedTally, vote);
+        addressAlreadyVoted[msg.sender] = true;
+        proofHashAlreadySeen[proofHash] = true;
+
+        emit EncryptedVoteCast(msg.sender);
+    }
+
+    /// @notice Stop the voting phase. Can only be called by authority.
+    function stopVotingPhase() external {
+        if (status != Status.VOTING) {
+            revert WrongStatus(status);
+        }
+        if (msg.sender != authority) {
+            revert Unauthorized(msg.sender);
+        }
+
+        finalizeTally(encryptedTally);
+        status = Status.TALLYING;
+
+        emit VotingStopped(encryptedTally);
+    }
+
+    /// @notice Announce the result of tallying. Can be called by anyone, but requires knowledge
+    /// of the secret key behind public key pk in order to actually perform the tallying off-chain
+    /// and produce a valid proof of correct decryption.
+    /// @param decryptedTally The result of decrypting the encrypted tally
+    /// @param proof The proof of correct decryption of the encrypted tally. This is generated
+    /// off-chain by the cryptographic backend.
+    function tally(
+        ProofCorrectDecryption calldata proof,
+        uint decryptedTally
+    ) external {
+        if (status != Status.TALLYING) {
+            revert WrongStatus(status);
+        }
+        verifyCorrectDecryption(proof, encryptedTally.vote, decryptedTally, pk);
+
+        result = decryptedTally;
+        status = Status.FINI;
+
+        emit Result(result);
+    }
+
+    /// @notice A getter for the public key of the voting event. Can only be called after the
+    /// public key has been declared.
+    function getPk() external view returns (CurvePoint memory pk_) {
+        if (status == Status.INIT) {
+            revert WrongStatus(status);
+        }
+
+        pk_ = pk;
+    }
+
+    /// @notice A getter for the result of the voting event. Can only be called after tallying.
+    function getResult() external view returns (uint result_) {
+        if (status != Status.FINI) {
+            revert WrongStatus(status);
+        }
+
+        result_ = result;
+    }
+}
