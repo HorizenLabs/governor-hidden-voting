ab03660049cd5b7e38eb8df4120a12b635d1867a
Luca Giussani <lucagiussani@horizenlabs.io>
2023-06-26 10:06:37 +0200

    Comment DiscretizedVotes contract

--- smart-contracts/contracts/openzeppelin-voting/DiscretizedVotes.sol
+++ smart-contracts/contracts/openzeppelin-voting/DiscretizedVotes.sol
@@ -4,6 +4,14 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/governance/utils/IVotes.sol";
 
+/**
+ * @dev Discretize the voting power of an {IVotes} instance using a decorator pattern.
+ * Voting power and total supply are divided by the quantity minWeight, which is declared
+ * at deployment. In this way voting power is always a multiple of minWeight. This allows
+ * using ERC-20 tokens for private voting: if using an ERC-20 token directly, with full
+ * resolution of voting power, then tallying could become very inefficient, due to the need
+ * to solve a discrete log with a big upper bound (i.e. the total weight cast).
+ */
 contract DiscretizedVotes is IVotes {
     IVotes private immutable _token;
     uint256 public minWeight;
